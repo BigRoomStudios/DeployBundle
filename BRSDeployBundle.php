2cbf2b1e2b3a4165f5695056aa5cc1866526ed0f
Sam Mateosian <sam@bigroomstudios.com>
2013-02-19 21:05:42 +0000

    creating deploy bundle

---
+++ BRSDeployBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace BRS\DeployBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BRSDeployBundle extends Bundle
+{
+}
---
+++ Command/DeployBundleCommand.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace BRS\DeployBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class DeployBundleCommand extends ContainerAwareCommand
+{
+	protected function configure()
+	{
+		$this
+			->setName('deploy')
+			->setDescription('update production cache and assets')
+			->addArgument('target', InputArgument::OPTIONAL, 'Specify a deploy target, default is this application.');
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		//$wd = exec('pwd');
+
+		//passthru("find . -name '*~' -type f -delete");
+		
+		$target = $input->getArgument('target');
+		
+		if($target){
+			
+			$targets = $this->getContainer()->getParameter('deploy_targets');
+			
+			if(isset($targets[$target])){
+				
+				$output->writeln("Deploying to $target");
+				
+				$target_info = $targets[$target];
+				
+				$ssh = $target_info['ssh'];
+				
+				$app = $target_info['app'];
+				
+				$output->writeln($ssh . ":" . $app);
+				
+				$command = "ssh $ssh 'cd $app; app/console deploy'";
+				
+				passthru($command);
+								
+			}else{
+				
+				$output->writeln("No target configured: $target");
+			}
+				
+		}else{
+			
+			$output->writeln("Deploying");
+			
+			passthru("git pull");
+			passthru("app/console src:update");
+			passthru("app/console assets:install --symlink web");
+			passthru("app/console assetic:dump --env=prod");
+			passthru("app/console cache:clear --env=prod --warmup");
+		}
+		
+		
+		
+	}
+}
